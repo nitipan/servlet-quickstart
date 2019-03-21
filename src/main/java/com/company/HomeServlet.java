bac8ef473f18d0150d70c281c26870ab84d5bbca
Nitipan Pompan <nitipan@Nitipans-MacBook-Pro.local>
2019-03-21 10:48:05 +0700

    fix isEmpty() not available on java 1.8

--- src/main/java/com/company/HomeServlet.java
+++ src/main/java/com/company/HomeServlet.java
@@ -1,9 +1,11 @@
 package com.company;
 
 import java.io.IOException;
+import java.util.Optional;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,7 +13,20 @@ import javax.servlet.http.HttpServletResponse;
 public class HomeServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.setAttribute("name", "Codecamp 3");
+
+        Optional<Cookie> cookieOptional = Optional.empty();
+        for (Cookie cookie : req.getCookies()) {
+            if (cookie.getName().equals("username")) {
+                cookieOptional = Optional.of(cookie);
+                break;
+            }
+        }
+        Cookie usernameCookie;
+        if (!cookieOptional.isPresent()) {
+            usernameCookie = new Cookie("username", "cc3user1");
+            usernameCookie.setMaxAge(600);
+            resp.addCookie(usernameCookie);
+        }
         req.getRequestDispatcher("/jsp/home.jsp").forward(req, resp);
     }
 }
