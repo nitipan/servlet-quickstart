52a3aba2161bb956459fad07c7b20439c606ade5
Nitipan Pompan <nitipan@Nitipans-MacBook-Pro.local>
2019-03-23 20:22:29 +0700

    cleancode

--- src/main/java/com/company/HomeServlet.java
+++ src/main/java/com/company/HomeServlet.java
@@ -1,11 +1,8 @@
 package com.company;
 
 import java.io.IOException;
-import java.util.Optional;
 
 import javax.servlet.ServletException;
-import javax.servlet.ServletOutputStream;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,20 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 public class HomeServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-        Optional<Cookie> cookieOptional = Optional.empty();
-        for (Cookie cookie : req.getCookies()) {
-            if (cookie.getName().equals("username")) {
-                cookieOptional = Optional.of(cookie);
-                break;
-            }
-        }
-        Cookie usernameCookie;
-        if (!cookieOptional.isPresent()) {
-            usernameCookie = new Cookie("username", "cc3user1");
-            usernameCookie.setMaxAge(600);
-            resp.addCookie(usernameCookie);
-        }
+        req.setAttribute("name", "Codecamp 3!");
         req.getRequestDispatcher("/jsp/home.jsp").forward(req, resp);
     }
 }
